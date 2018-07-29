a929af4464c9ddcb232583de0b932af9e7e3057b
Luiz Fernando <38297606+lfnd0@users.noreply.github.com>
2018-07-29 02:13:11 -0300

    Códigos dos "Testes de conhecimento inicial"

--- testes_de_conhecimento_inicial/e_um_numero_triangular.py
+++ testes_de_conhecimento_inicial/e_um_numero_triangular.py
@@ -3,12 +3,12 @@ i = 1
 t = 0
 
 while (t < n1):
-	t = i*(i+1)*(i+2)
-	i += 1
+    t = i*(i+1)*(i+2)
+    i += 1
 
 if (t == n1 and n1 != 0):
-	print (i-1,"*",(i),"*",(i+1), "=", n1)
-	print ("Verdadeiro")
+    print (i-1,"*",(i),"*",(i+1), "=", n1)
+    print ("Verdadeiro")
 
 else: 
-	print ("Falso")
+    print ("Falso")
