49241b9860b57136dc82a0d312995dc6633155c1
Luiz Fernando <38297606+lfnd0@users.noreply.github.com>
2018-08-02 22:26:18 -0300

    Atividade: "Condicionais e estruturas de repetição"
    Modificações nos códigos...

--- condicionais_e_estruturas_de_repeticao/matriculas_duplas.py
+++ condicionais_e_estruturas_de_repeticao/matriculas_duplas.py
@@ -1,17 +1,16 @@
 p2 = input()
 p2 = p2.split(' ')
-p2 = list(map(int, p2))
+p2 = list(map(str, p2))
 
 p3 = input()
 p3 = p3.split(' ')
-p3 = list(map(int, p3))
+p3 = list(map(str, p3))
 
 duplicado = []
+n = ''
 
 for aluno_p2 in p2:
     for aluno_p3 in p3:
-        if aluno_p2==aluno_p3 and aluno_p2 not in duplicado:
-            duplicado.append(aluno_p2)
-            break
-duplicado = str(duplicado).strip('[]').replace(',', '')
-print(duplicado)
+        if aluno_p2 == aluno_p3:
+            n = n + str(aluno_p2) + ' '
+print(n)
